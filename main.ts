5d5d34a19fbadc8086aa67f8ef8728765b04fcd8
JaysonGonzales1272009 <125311522+JaysonGonzales1272009@users.noreply.github.com>
2023-02-22 09:41:48 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
 basic.forever(function () {
-	
+    if (true) {
+        basic.showString("derek is....... GAY")
+    } else {
+        images.createBigImage(`
+            # . . . . . . . . #
+            . # . . . . . . # .
+            . . # . . . . # . .
+            . . . # . . # . . .
+            . . . . # # . . . .
+            `).scrollImage(1, 200)
+    }
 })
